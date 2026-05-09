7d0e591edbfb9af6a2fcd4942465a8eb7fe4cc37
wela-herveyyy <hervey.geralph@livro.systems>
2026-05-09 23:20:06 +0800

    [feat]: transfer /app/component to /component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,34 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Nav from "./components/organisms/Nav";
+import Nav from "../components/organisms/Nav";
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Herv.Web | Specialized System Engineer",
-    description: "Engineering Custom Web Application Systems",
-    icons: {
-        icon: "/logo.svg",
-    },
+  title: "Herv.Web | Specialized System Engineer",
+  description: "Engineering Custom Web Application Systems",
+  icons: {
+    icon: "/logo.svg",
+  },
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" className="scroll-smooth sm:scroll-auto">
-            <body
-                className={`${inter.className} antialiased selection:bg-emerald-100 selection:text-emerald-900 `}
-            >
-                <Nav />
-                {children}
-                <Script src="https://cdn.tailwindcss.com" />
-                <Script src="https://cdn.jsdelivr.net/npm/chart.js" />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" className="scroll-smooth sm:scroll-auto">
+      <body
+        className={`${inter.className} antialiased selection:bg-emerald-100 selection:text-emerald-900 `}
+      >
+        <Nav />
+        {children}
+        <Script src="https://cdn.tailwindcss.com" />
+        <Script src="https://cdn.jsdelivr.net/npm/chart.js" />
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Architecture from "./components/organisms/Architecture";
-import CTA from "./components/organisms/CTA";
-import Footer from "./components/organisms/Footer";
-import Hero from "./components/organisms/Hero";
-import Value from "./components/organisms/Value";
+import Architecture from "../components/organisms/Architecture";
+import CTA from "../components/organisms/CTA";
+import Footer from "../components/organisms/Footer";
+import Hero from "../components/organisms/Hero";
+import Value from "../components/organisms/Value";
 
 export default function Home() {
-    return (
-        <main className="pt-32 ">
-            <Hero />
-            <Architecture />
-            <Value />
-            <CTA />
-            <Footer />
-        </main>
-    );
+  return (
+    <main className="pt-32 ">
+      <Hero />
+      <Architecture />
+      <Value />
+      <CTA />
+      <Footer />
+    </main>
+  );
 }
